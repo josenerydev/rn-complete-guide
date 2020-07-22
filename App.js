ceee667c3c2363a8d32963b06b53647deea885cf
José Nery <josenerydev@gmail.com>
2020-07-22 20:36:36 -0300

    45. Handling Error Messages

--- App.js
+++ App.js
@@ -9,11 +9,16 @@ export default function App() {
   const [isAddMode, setIsAddMode] = useState(false);
 
   const addGoalHandler = goalTitle => {
-    setCourseGoals(currentGoals => [...currentGoals,
-    {
-      id: Math.random().toString(),
-      value: goalTitle
-    }]);
+    if (goalTitle.length === 0) {
+      return;
+    }
+
+    setCourseGoals(currentGoals => [
+      ...currentGoals,
+      {
+        id: Math.random().toString(),
+        value: goalTitle
+      }]);
     setIsAddMode(false);
   };
 
