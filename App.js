52fb67da8161d1c27d76f602fabd13412590161b
José Nery <josenerydev@gmail.com>
2020-07-22 11:39:08 -0300

    30. Making it Scrollable with ScrollView!

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState('');
@@ -24,9 +24,13 @@ export default function App() {
         />
         <Button title="ADD" onPress={addGoalHandler} />
       </View>
-      <View>
-        {courseGoals.map((goal, index) => <View style={styles.listItem} key={index}><Text>{goal}</Text></View>)}
-      </View>
+      <ScrollView>
+        {courseGoals.map((goal, index) => (
+          <View style={styles.listItem} key={index}>
+            <Text>{goal}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
